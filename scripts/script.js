03c46378fd46abcfeb76eb1959d3c4c328bcef71
kimvanvelzen21 <70523185+kimvanvelzen21@users.noreply.github.com>
2020-09-30 17:22:28 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,37 @@
-// JavaScript Document
+// JavaScript
+var klapUit = document.querySelector("header img:first-of-type");
+var klapIn = document.querySelector(".menu img");
+var body = document.querySelector("body");
+
+var menu = document.querySelector(".menu");
+
+klapUit.addEventListener("click", uitklappen);
+klapIn.addEventListener("click", inklappen);
+
+function uitklappen() {
+    menu.classList.add("toonMenu");
+    body.classList.add("absolute");
+}
+
+function inklappen() {
+    menu.classList.remove("toonMenu");
+    body.classList.remove("absolute");
+}
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("slides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
